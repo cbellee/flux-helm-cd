16d5815c1fe97587c5be343b7a8bd049e92c9f86
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2020-02-21 10:51:48 +1100

    modified scritp

--- scripts/install-flux-helm-operators.sh
+++ scripts/install-flux-helm-operators.sh
@@ -1,11 +1,11 @@
 helm repo add fluxcd https://charts.fluxcd.io
 kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/flux-helm-release-crd.yaml
 
-helm upgrade -i flux fluxcd/flux --wait \
+helm install flux fluxcd/flux --wait \
 --namespace fluxcd \
 --set git.url=git@github.com:cbellee/flux-helm-cd \
---set git.path="clusters/common\,clusters/aks-3acf67" \
---set git.label=aks-3acf67 \
+--set git.path="clusters/dev" \
+--set git.label=dev \
 --set additionalArgs={--sync-garbage-collection}
 
 
