c6ef615e1c8cbe87dfd358a48fbcd012b8f284c9
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2020-02-22 21:19:07 +1100

    added script to install nginx ingress

---
+++ scripts/install-ingress-controller.sh
@@ -0,0 +1,4 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.29.0/deploy/static/provider/cloud-generic.yaml
+
+# Helm install
+# helm install stable/nginx-ingress --name my-nginx
