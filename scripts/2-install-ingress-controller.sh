a5cceafd6d3a1adee73a14790e09877ab4875a15
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2020-02-24 21:35:58 +1100

    added acr secret

--- scripts/2-install-ingress-controller.sh
+++ scripts/2-install-ingress-controller.sh
@@ -1,4 +1,4 @@
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.29.0/deploy/static/mandatory.yaml
 helm install go-nginx stable/nginx-ingress
 
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.29.0/deploy/static/mandatory.yaml
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.29.0/deploy/static/provider/cloud-generic.yaml
---
+++ scripts/4-create-acr-secret.sh
@@ -0,0 +1,14 @@
+# assumes ACR Admin Account is enabled
+
+ACR_NAME=acrd8f374
+ACR_FULL_NAME="${ACR_NAME}.azurecr.io"
+
+ACR_UNAME=$(az acr credential show -n $ACR_NAME --query="username" -o tsv)
+ACR_PASSWD=$(az acr credential show -n $ACR_NAME --query="passwords[0].value" -o tsv)
+
+kubectl create secret docker-registry acr-secret \
+  --docker-server="${ACR_FULL_NAME}" \
+  --docker-username=$ACR_UNAME \
+  --docker-password=$ACR_PASSWD \
+  --docker-email=ignorethis@email.com
+  
