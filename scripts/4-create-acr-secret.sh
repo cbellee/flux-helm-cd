71050d329ba69be73bc5371b983c2f1371ff0960
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2020-02-24 22:26:53 +1100

    fixed formatting issue

--- scripts/4-create-acr-secret.sh
+++ scripts/4-create-acr-secret.sh
@@ -7,8 +7,7 @@ ACR_UNAME=$(az acr credential show -n $ACR_NAME --query="username" -o tsv)
 ACR_PASSWD=$(az acr credential show -n $ACR_NAME --query="passwords[0].value" -o tsv)
 
 kubectl create secret docker-registry acr-secret \
-  --docker-server="${ACR_FULL_NAME}" \
+  --docker-server=$ACR_FULL_NAME \
   --docker-username=$ACR_UNAME \
   --docker-password=$ACR_PASSWD \
   --docker-email=ignorethis@email.com
-  
